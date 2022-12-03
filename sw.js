352a407277c683863efeb600be94243e24686661
karinaG343 <83309631+karinaG343@users.noreply.github.com>
2022-12-02 18:40:30 -0600

    Avance final

--- sw.js
+++ sw.js
@@ -1,23 +1,27 @@
-self.addEventListener("install", (e) => {
-  caches.open("cache-v2").then((cache) => {
-    cache.addAll([
-      'css/style.css',
-      'images/1.png',
-      'images/2.png',
-      'images/3.png',
-      'images/4.png',
-      'images/5.png',
-      'images/6.png',
-      'images/7.png',
-      'images/facebook.png',
-      'images/instagram.png',
-      'images/twiter.png',
-      'videos/1.mp4',
-      'videos/2.mp4',
-      'videos/3.mp4',
-      'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'
-    ]);
-  });
+self.addEventListener('install', e =>{
+  const cacheProm = caches.open('cache-v1')
+      .then(cache => {
+          return cache.addAll([
+            
+              'index.html',
+              'css/style.css',
+              'css/style.css',
+              'images/1.png',
+              'images/2.png',
+              'images/3.png',
+              'images/4.png',
+              'images/5.png',
+              'images/6.png',
+              'images/7.png',
+              'images/facebook.png',
+              'images/instagram.png',
+              'images/twiter.png',
+              'videos/1.mp4',
+              'videos/2.mp4',
+              'videos/3.mp4',
+          ])
+          
+      });
   e.waitUntil(cacheProm);
 });
 
@@ -30,7 +34,7 @@ self.addEventListener('fetch', e =>{
           //tengo que ir a la web
           console.log('No existe', e.request.url);
           return fetch( e.request ).then ( newResp => {
-              caches.open('cache-v2')
+              caches.open('cache-v1')
                   .then( cache => {
                       cache.put( e.request, newResp);
                   }
